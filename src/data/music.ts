b48f84fd62932da91d1a8bb733efe56f86471ee2
Pa7echnik <morpech2025@gmail.com>
2026-09-02 14:54:37 +0900

    Update music.ts

--- src/data/music.ts
+++ src/data/music.ts
@@ -16,7 +16,6 @@ export const SEED_TRACKS: MusicTrack[] = [
     boundLocationId: "loc-moonport",
   },
   {
-  {
     id: "tr-moonport",
     title: "Тема Лунного порта",
     subtitle: "основная тема города",
